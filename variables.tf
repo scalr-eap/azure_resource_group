83458952edea29c343a2cbd6c0fde2bcfbcca542
Ryan <ryan.fee625@gmail.com>
2020-03-04 10:45:23 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
 variable "region" {}
 
 variable "name" {}
-
-variable "scalr_azurerm_subscription_id" {}
-
-variable "scalr_azurerm_client_id" {}
-
-variable "scalr_azurerm_client_secret" {}
-
-variable "scalr_azurerm_tenant_id" {}
